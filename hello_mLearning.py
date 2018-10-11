657f39d7b56aa8a1a5ff8aaaa05de8849d8e9d55
Neo <hwhsong@163.com>
2018-10-11 10:38:06 +0800

    十行代码感知机器学习

---
+++ hello_mLearning.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""十行代码感知机器学习"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 原始数据
+X = [1, 2, 3, 4, 5, 6]
+Y = [2.6, 3.4, 4.7, 5.5, 6.47, 7.8]
+
+# 用一次多项式拟合，相当于线性拟合
+z1 = np.polyfit(X, Y, 1)
+p1 = np.poly1d(z1)
+
+# 作图显示
+x = np.arange(1, 7)
+y = z1[0] * x + z1[1]
+plt.figure()
+plt.scatter(X, Y)
+plt.plot(x, y)
+plt.show()
